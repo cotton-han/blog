7f3d4688e9d9a1bc795eea45276a57cd98a60227
YEJI HAN <yeji9175@naver.com>
2021-12-01 00:54:29 +0900

    feat: Button 컴포넌트 구현 및 적용

--- frontend/src/pages/PostListPage.js
+++ frontend/src/pages/PostListPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 function PostListPage() {
   return (
     <div>
-      <div>PostListPage</div>
+      <Button>버튼</Button>
     </div>
   );
 }
