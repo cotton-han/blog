362046b656ddccc09201c3697a4db9c7d4127d8c
YEJI HAN <yeji9175@naver.com>
2021-11-30 17:37:41 +0900

    feat: 페이지네이션 구현

--- backend/src/api/posts/posts.controller.js
+++ backend/src/api/posts/posts.controller.js
@@ -1,4 +1,4 @@
-import Post from '../../models/posts';
+import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from '@hapi/joi';
 
@@ -53,9 +53,26 @@ export const write = async ctx => {
 GET /api/posts
 */
 export const list = async ctx => {
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean() // JSON 변환 불필요
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts.map(post => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
