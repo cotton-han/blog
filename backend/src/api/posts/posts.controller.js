668a25acdf9f2ab73f27ca83be917e7d1dd0a75f
YEJI HAN <yeji9175@naver.com>
2021-11-30 17:00:06 +0900

    feat: Request Body 검증
    - @hapi/joi 라이브러리 활용

--- backend/src/api/posts/posts.controller.js
+++ backend/src/api/posts/posts.controller.js
@@ -1,5 +1,6 @@
 import Post from '../../models/posts';
 import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -21,6 +22,19 @@ POST /api/posts
 }
 */
 export const write = async ctx => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -87,6 +101,20 @@ PATCH /api/posts/:id
 */
 export const update = async ctx => {
   const { id } = ctx.params;
+
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 업데이트된 데이터를 반환
